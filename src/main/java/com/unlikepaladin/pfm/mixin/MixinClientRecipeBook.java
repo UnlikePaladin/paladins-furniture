18b26a2c08a007e9d51c824a98b1cbf8aec6e2e8
UnlikePaladin <36827970+UnlikePaladin@users.noreply.github.com>
2022-06-13 23:23:00 -0500

    Fixed Unknown Recipe Type log spam upon loading world

---
+++ src/main/java/com/unlikepaladin/pfm/mixin/MixinClientRecipeBook.java
@@ -0,0 +1,25 @@
+package com.unlikepaladin.pfm.mixin;
+
+import com.unlikepaladin.pfm.PaladinFurnitureMod;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.recipebook.ClientRecipeBook;
+import net.minecraft.client.recipebook.RecipeBookGroup;
+import net.minecraft.recipe.Recipe;
+import net.minecraft.recipe.RecipeType;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Environment(EnvType.CLIENT)
+@Mixin(ClientRecipeBook.class)
+public abstract class MixinClientRecipeBook {
+    @Inject(method = "getGroupForRecipe", at = @At(value = "HEAD"), cancellable = true)
+    private static void getGroupForRecipe(Recipe<?> recipe, CallbackInfoReturnable<RecipeBookGroup> callbackInfoReturnable) {
+        RecipeType<?> type = recipe.getType();
+        if (type == PaladinFurnitureMod.FREEZING_RECIPE || type == PaladinFurnitureMod.FURNITURE_RECIPE) {
+            callbackInfoReturnable.setReturnValue(RecipeBookGroup.UNKNOWN);
+        }
+    }
+}
