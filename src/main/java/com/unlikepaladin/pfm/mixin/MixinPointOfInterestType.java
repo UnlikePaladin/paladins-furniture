8474252f2dcc547dc1ec0f4ec4fcedb8f2651da7
UnlikePaladin <36827970+UnlikePaladin@users.noreply.github.com>
2022-07-17 18:52:57 +0200

    Fixed the Point of Interest in 1.19, that was painful Thanks to LlamaLad7#7444 for helping me out with the mixin

--- src/main/java/com/unlikepaladin/pfm/mixin/MixinPointOfInterestType.java
+++ src/main/java/com/unlikepaladin/pfm/mixin/MixinPointOfInterestType.java
@@ -5,7 +5,7 @@ import com.unlikepaladin.pfm.blocks.SimpleBed;
 import com.unlikepaladin.pfm.registry.BlockItemRegistry;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.enums.BedPart;
-import net.minecraft.world.poi.PointOfInterestType;
+import net.minecraft.world.poi.PointOfInterestTypes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArgs;
@@ -17,31 +17,31 @@ import java.util.HashSet;
 import java.util.Set;
 
 
-@Mixin(PointOfInterestType.class)
+@Mixin(PointOfInterestTypes.class)
 public abstract class MixinPointOfInterestType {
     @ModifyArgs(
-            method = "<clinit>",
+            method = "registerAndGetDefault(Lnet/minecraft/util/registry/Registry;)Lnet/minecraft/world/poi/PointOfInterestType;",
             slice = @Slice(from = @At(
-                    value = "CONSTANT",
-                    args = "stringValue=home")
-            ),
+                    value = "FIELD",
+                    target = "Lnet/minecraft/world/poi/PointOfInterestTypes;HOME:Lnet/minecraft/util/registry/RegistryKey;"
+            )),
             at = @At(
                     value = "INVOKE",
-                    target = "Lnet/minecraft/world/poi/PointOfInterestType;register(Ljava/lang/String;Ljava/util/Set;II)Lnet/minecraft/world/poi/PointOfInterestType;",
+                    target = "Lnet/minecraft/world/poi/PointOfInterestTypes;register(Lnet/minecraft/util/registry/Registry;Lnet/minecraft/util/registry/RegistryKey;Ljava/util/Set;II)Lnet/minecraft/world/poi/PointOfInterestType;",
                     ordinal = 0
             )
     )
     private static void appendBeds(Args args) {
-        Set<BlockState> originalBedStates = args.get(1);
+        Set<BlockState> originalBedStates = args.get(2);
         Set<BlockState> addedBedStates = Arrays.stream(BlockItemRegistry.getBeds()).flatMap(block -> block.getStateManager().getStates().stream().filter(state -> state.get(SimpleBed.PART) == BedPart.HEAD)).collect(ImmutableSet.toImmutableSet());
         Set<BlockState> newBedStates = new HashSet<>();
         newBedStates.addAll(originalBedStates);
         newBedStates.addAll(addedBedStates);
         newBedStates = newBedStates.stream().collect(ImmutableSet.toImmutableSet());
         // Add new blockStates
-        args.set(1, newBedStates);
+        args.set(2, newBedStates);
         // Set ticketCount
-        args.set(2, 1);
+        args.set(3, 1);
 
     }
 }
