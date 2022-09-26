43854007b4c46e802e5cd8b97496ce158f1db894
UnlikePaladin <36827970+UnlikePaladin@users.noreply.github.com>
2022-09-26 18:29:13 -0500

    1.18 changes and fixes

--- common/src/main/java/com/unlikepaladin/pfm/blocks/blockentities/TrashcanBlockEntity.java
+++ common/src/main/java/com/unlikepaladin/pfm/blocks/blockentities/TrashcanBlockEntity.java
@@ -106,12 +106,11 @@ public class TrashcanBlockEntity extends LootableContainerBlockEntity {
     }
 
     @Override
-    public NbtCompound writeNbt(NbtCompound nbt) {
+    public void writeNbt(NbtCompound nbt) {
         super.writeNbt(nbt);
         if (!this.serializeLootTable(nbt)) {
             Inventories.writeNbt(nbt, this.inventory);
         }
-        return nbt;
     }
 
     public DefaultedList<ItemStack> getInventory() {
