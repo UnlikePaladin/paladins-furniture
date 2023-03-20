a20c99328fbee279b21f28b38a587c32acc81188
UnlikePaladin <36827970+UnlikePaladin@users.noreply.github.com>
2023-03-19 23:36:30 -0600

    Mirror Model now works and uses a Bitset on Forge

--- forge/src/main/java/com/unlikepaladin/pfm/blocks/models/forge/ForgeMirrorModel.java
+++ forge/src/main/java/com/unlikepaladin/pfm/blocks/models/forge/ForgeMirrorModel.java
@@ -12,62 +12,52 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.world.BlockRenderView;
-import net.minecraft.world.BlockView;
 import net.minecraftforge.client.model.data.IModelData;
+import net.minecraftforge.client.model.data.ModelDataMap;
 import net.minecraftforge.client.model.data.ModelProperty;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Random;
+import java.util.*;
+import java.util.function.Predicate;
 
 public class ForgeMirrorModel extends BakedMirrorModel {
     public ForgeMirrorModel(Sprite frame, Map<BlockState, Sprite> frameOverrides, Sprite glassTex, Sprite reflectTex, ModelBakeSettings settings, Map<Identifier, BakedModel> bakedModels) {
         super(frame, frameOverrides, glassTex, reflectTex, settings, bakedModels);
     }
 
-    public static final ModelProperty<Boolean> up = new ModelProperty<>();
-    public static final ModelProperty<Boolean> down = new ModelProperty<>();
-    public static final ModelProperty<Boolean> left = new ModelProperty<>();
-    public static final ModelProperty<Boolean> right = new ModelProperty<>();
-
-    public static final ModelProperty<Boolean> cornerLeftUp = new ModelProperty<>();
-    public static final ModelProperty<Boolean> cornerRightDown = new ModelProperty<>();
-    public static final ModelProperty<Boolean> cornerLeftDown = new ModelProperty<>();
-    public static final ModelProperty<Boolean> cornerRightUp = new ModelProperty<>();
+    public static ModelProperty<MirrorDirections> DIRECTIONS = new ModelProperty<>();
 
 
     @NotNull
     @Override
     public List<BakedQuad> getQuads(@Nullable BlockState state, @Nullable Direction side, @NotNull Random rand, @NotNull IModelData extraData) {
         List<BakedQuad> quads = new ArrayList<>(getBakedModels().get(MirrorUnbakedModel.MODEL_MIRROR_GLASS).getQuads(state, side, rand, extraData));
-        if (state.getBlock() instanceof MirrorBlock && extraData.hasProperty(up) && extraData.hasProperty(down) && extraData.hasProperty(left) && extraData.hasProperty(right) && extraData.hasProperty(cornerLeftDown) && extraData.hasProperty(cornerLeftUp) && extraData.hasProperty(cornerRightUp) && extraData.hasProperty(cornerRightDown)) {
-            if (!extraData.getData(down)) {
-                quads.addAll(getBakedModels().get(MirrorUnbakedModel.MODEL_MIRROR_BOTTOM).getQuads(state, side, rand, extraData));
-            }
-            if (!extraData.getData(up)) {
+        if (state.getBlock() instanceof MirrorBlock) {
+            BitSet connections = extraData.getData(DIRECTIONS).connections;
+            if (!connections.get(0)) {
                 quads.addAll(getBakedModels().get(MirrorUnbakedModel.MODEL_MIRROR_TOP).getQuads(state, side, rand, extraData));
             }
-            if (!extraData.getData(right)) {
-                quads.addAll(getBakedModels().get(MirrorUnbakedModel.MODEL_MIRROR_LEFT).getQuads(state, side, rand, extraData));
+            if (!connections.get(1)) {
+                quads.addAll(getBakedModels().get(MirrorUnbakedModel.MODEL_MIRROR_BOTTOM).getQuads(state, side, rand, extraData));
             }
-            if (!extraData.getData(left)) {
+            if (!connections.get(2)) {
                 quads.addAll(getBakedModels().get(MirrorUnbakedModel.MODEL_MIRROR_RIGHT).getQuads(state, side, rand, extraData));
             }
-
-            if (!extraData.getData(cornerLeftDown)) {
-                quads.addAll(getBakedModels().get(MirrorUnbakedModel.MODEL_MIRROR_LEFT_BOTTOM_CORNER).getQuads(state, side, rand, extraData));
+            if (!connections.get(3)) {
+                quads.addAll(getBakedModels().get(MirrorUnbakedModel.MODEL_MIRROR_LEFT).getQuads(state, side, rand, extraData));
             }
-            if (!extraData.getData(cornerRightDown)) {
-                quads.addAll(getBakedModels().get(MirrorUnbakedModel.MODEL_MIRROR_RIGHT_BOTTOM_CORNER).getQuads(state, side, rand, extraData));
-            }
-            if (!extraData.getData(cornerLeftUp)) {
+            if (!connections.get(4)) {
                 quads.addAll(getBakedModels().get(MirrorUnbakedModel.MODEL_MIRROR_LEFT_TOP_CORNER).getQuads(state, side, rand, extraData));
             }
-            if (!extraData.getData(cornerRightUp)) {
+            if (!connections.get(5)) {
+                quads.addAll(getBakedModels().get(MirrorUnbakedModel.MODEL_MIRROR_LEFT_BOTTOM_CORNER).getQuads(state, side, rand, extraData));
+            }
+            if (!connections.get(6)) {
                 quads.addAll(getBakedModels().get(MirrorUnbakedModel.MODEL_MIRROR_RIGHT_TOP_CORNER).getQuads(state, side, rand, extraData));
+            }
+            if (!connections.get(7)) {
+                quads.addAll(getBakedModels().get(MirrorUnbakedModel.MODEL_MIRROR_RIGHT_BOTTOM_CORNER).getQuads(state, side, rand, extraData));
             }
         }
         return quads;
@@ -76,18 +66,36 @@ public class ForgeMirrorModel extends BakedMirrorModel {
     @NotNull
     @Override
     public IModelData getModelData(@NotNull BlockRenderView blockView, @NotNull BlockPos pos, @NotNull BlockState state, @NotNull IModelData tileData) {
+        ModelDataMap.Builder builder = new ModelDataMap.Builder();
         if (state.getBlock() instanceof MirrorBlock) {
             MirrorBlock block = (MirrorBlock) state.getBlock();
             Direction facing = state.get(MirrorBlock.FACING);
-            tileData.setData(up, block.canConnect(blockView.getBlockState(pos.up()), state));
-            tileData.setData(down, block.canConnect(blockView.getBlockState(pos.down()), state));
-            tileData.setData(left, block.canConnect(blockView.getBlockState(pos.offset(facing.rotateYClockwise())), state));
-            tileData.setData(right, block.canConnect(blockView.getBlockState(pos.offset(facing.rotateYCounterclockwise())), state));
-            tileData.setData(cornerLeftUp, block.canConnect(blockView.getBlockState(pos.offset(facing.rotateYClockwise()).up()), state));
-            tileData.setData(cornerRightDown, block.canConnect(blockView.getBlockState(pos.offset(facing.rotateYCounterclockwise()).down()), state));
-            tileData.setData(cornerLeftDown, block.canConnect(blockView.getBlockState(pos.offset(facing.rotateYClockwise()).down()), state));
-            tileData.setData(cornerRightUp, block.canConnect(blockView.getBlockState(pos.offset(facing.rotateYCounterclockwise()).up()), state));
+            BitSet connections = new BitSet(8);
+            connections.set(0, block.canConnect(blockView.getBlockState(pos.up()), state));
+            connections.set(1, block.canConnect(blockView.getBlockState(pos.down()), state));
+            connections.set(2, block.canConnect(blockView.getBlockState(pos.offset(facing.rotateYClockwise())), state));
+            connections.set(3, block.canConnect(blockView.getBlockState(pos.offset(facing.rotateYCounterclockwise())), state));
+
+            connections.set(4, block.canConnect(blockView.getBlockState(pos.offset(facing.rotateYClockwise()).up()), state));
+            connections.set(5, block.canConnect(blockView.getBlockState(pos.offset(facing.rotateYClockwise()).down()), state));
+            connections.set(6, block.canConnect(blockView.getBlockState(pos.offset(facing.rotateYCounterclockwise()).up()), state));
+            connections.set(7, block.canConnect(blockView.getBlockState(pos.offset(facing.rotateYCounterclockwise()).down()), state));
+            MirrorDirections mirrorDirections = new MirrorDirections(connections);
+            builder.withInitial(DIRECTIONS, mirrorDirections);
         }
-        return tileData;
+        return builder.build();
+    }
+}
+
+class MirrorDirections implements Predicate<MirrorDirections>
+{
+    public MirrorDirections(BitSet connections) {
+        this.connections = connections;
+    }
+
+    protected BitSet connections;
+    @Override
+    public boolean test(MirrorDirections mirrorDirections) {
+        return connections.equals(mirrorDirections.connections);
     }
 }
