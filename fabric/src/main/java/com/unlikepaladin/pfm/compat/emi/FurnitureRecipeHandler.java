c3fb7bb6054f655fe16c7ca3f3c360ca847054d6
UnlikePaladin <36827970+UnlikePaladin@users.noreply.github.com>
2023-08-06 21:48:02 -0600

    Fix EMI Plugin causing crashes

--- fabric/src/main/java/com/unlikepaladin/pfm/compat/emi/FurnitureRecipeHandler.java
+++ fabric/src/main/java/com/unlikepaladin/pfm/compat/emi/FurnitureRecipeHandler.java
@@ -2,8 +2,10 @@ package com.unlikepaladin.pfm.compat.emi;
 
 import com.google.common.collect.Lists;
 import com.unlikepaladin.pfm.menus.WorkbenchScreenHandler;
+import dev.emi.emi.api.EmiFillAction;
 import dev.emi.emi.api.EmiRecipeHandler;
 import dev.emi.emi.api.recipe.EmiRecipe;
+import net.minecraft.client.gui.screen.ingame.HandledScreen;
 import net.minecraft.screen.slot.Slot;
 import org.jetbrains.annotations.Nullable;
 
@@ -14,11 +16,8 @@ public class FurnitureRecipeHandler implements EmiRecipeHandler<WorkbenchScreenH
     @Override
     public List<Slot> getInputSources(WorkbenchScreenHandler handler) {
         List<Slot> list = Lists.newArrayList();
-        for (int i = 1; i < 10; i++) {
-            list.add(handler.getSlot(i));
-        }
-        int invStart = 10;
-        for (int i = invStart; i < invStart + 36; i++) {
+        int invStart = 1;
+        for(int i = invStart; i < invStart + 36; ++i) {
             list.add(handler.getSlot(i));
         }
         return list;
@@ -26,11 +25,7 @@ public class FurnitureRecipeHandler implements EmiRecipeHandler<WorkbenchScreenH
 
     @Override
     public List<Slot> getCraftingSlots(WorkbenchScreenHandler handler) {
-        List<Slot> list = Lists.newArrayList();
-        for (int i = 1; i < 10; i++) {
-            list.add(handler.getSlot(i));
-        }
-        return list;
+        return List.of();
     }
 
     @Override
@@ -41,5 +36,10 @@ public class FurnitureRecipeHandler implements EmiRecipeHandler<WorkbenchScreenH
     @Override
     public boolean supportsRecipe(EmiRecipe recipe) {
         return recipe.getCategory() == PaladinFurnitureModEMIPlugin.FURNITURE && recipe.supportsRecipeTree();
+    }
+
+    @Override
+    public boolean performFill(EmiRecipe recipe, HandledScreen<WorkbenchScreenHandler> screen, EmiFillAction action, int amount) {
+        return EmiRecipeHandler.super.performFill(recipe, screen, action, amount);
     }
 }
