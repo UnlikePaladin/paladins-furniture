190cea6dd92f4a0c7c156cb4dae7bb5bf9cb31a0
UnlikePaladin <36827970+UnlikePaladin@users.noreply.github.com>
2023-10-30 21:20:26 -0600

    Fix shower particle crashing on 1.19.4+ on Fabric

--- fabric/src/main/java/com/unlikepaladin/pfm/client/fabric/ParticleProviderRegistryFabric.java
+++ fabric/src/main/java/com/unlikepaladin/pfm/client/fabric/ParticleProviderRegistryFabric.java
@@ -4,12 +4,21 @@ import com.unlikepaladin.pfm.PaladinFurnitureMod;
 import com.unlikepaladin.pfm.registry.ParticleIDs;
 import net.fabricmc.fabric.api.client.particle.v1.ParticleFactoryRegistry;
 import net.minecraft.client.particle.BlockLeakParticle;
+import net.minecraft.client.particle.SpriteBillboardParticle;
 import net.minecraft.screen.PlayerScreenHandler;
 import net.minecraft.util.Identifier;
 
 public class ParticleProviderRegistryFabric {
 
     public static void registerParticleFactories() {
-        ParticleFactoryRegistry.getInstance().register(ParticleIDs.WATER_DROP, BlockLeakParticle::createFallingWater);
+        ParticleFactoryRegistry.getInstance().register(ParticleIDs.WATER_DROP, provider -> {
+            return (parameters, world, x, y, z, velocityX, velocityY, velocityZ) -> {
+                SpriteBillboardParticle spriteBillboardParticle = BlockLeakParticle.createFallingWater(parameters, world, x, y, z, velocityX, velocityY, velocityZ);
+                if (provider != null) {
+                    spriteBillboardParticle.setSprite(provider);
+                }
+                return spriteBillboardParticle;
+            };
+        });
     }
 }
