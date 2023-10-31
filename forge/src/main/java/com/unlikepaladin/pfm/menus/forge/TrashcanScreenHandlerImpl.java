b436a41118836e2eb9746e10baa491d93580cf15
UnlikePaladin <36827970+UnlikePaladin@users.noreply.github.com>
2023-10-31 16:05:15 -0600

    Fix Forge 1.20.2

--- forge/src/main/java/com/unlikepaladin/pfm/menus/forge/TrashcanScreenHandlerImpl.java
+++ forge/src/main/java/com/unlikepaladin/pfm/menus/forge/TrashcanScreenHandlerImpl.java
@@ -1,7 +1,6 @@
 package com.unlikepaladin.pfm.menus.forge;
 
 import com.unlikepaladin.pfm.blocks.blockentities.TrashcanBlockEntity;
-import com.unlikepaladin.pfm.networking.forge.MicrowaveActivePacket;
 import com.unlikepaladin.pfm.networking.forge.TrashcanClearPacket;
 import com.unlikepaladin.pfm.registry.forge.NetworkRegistryForge;
 import net.minecraft.client.MinecraftClient;
--- forge/src/main/java/com/unlikepaladin/pfm/registry/forge/NetworkRegistryForge.java
+++ forge/src/main/java/com/unlikepaladin/pfm/registry/forge/NetworkRegistryForge.java
@@ -4,31 +4,33 @@ import com.unlikepaladin.pfm.PaladinFurnitureMod;
 import com.unlikepaladin.pfm.advancements.PFMCriteria;
 import com.unlikepaladin.pfm.networking.forge.*;
 import io.netty.util.AttributeKey;
+import net.minecraft.server.network.ServerPlayerConfigurationTask;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.Identifier;
 import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraftforge.event.network.GatherLoginConfigurationTasksEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.network.*;
+import net.minecraftforge.network.config.SimpleConfigurationTask;
 
 @Mod.EventBusSubscriber(modid = "pfm", bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class NetworkRegistryForge {
 
-    private static final String PROTOCOL_VERSION = "1";
     public static final SimpleChannel PFM_CHANNEL = ChannelBuilder.named(
             new Identifier(PaladinFurnitureMod.MOD_ID, "main_channel")
-    ).networkProtocolVersion(Integer.parseInt(PROTOCOL_VERSION)).simpleChannel();
+    ).networkProtocolVersion(1).simpleChannel();
     public static final AttributeKey<ForgePacketHandler> CONTEXT = AttributeKey.newInstance("pfm:handshake");
 
 
     public static void registerPackets() {
         int id = 0;
 
-        PFM_CHANNEL.messageBuilder(MicrowaveUpdatePacket.class, ++id).encoder(MicrowaveUpdatePacket::encode).decoder(MicrowaveUpdatePacket::decode).consumerNetworkThread(CONTEXT, MicrowaveUpdatePacket::handle);
-        PFM_CHANNEL.messageBuilder(MicrowaveActivePacket.class, ++id).encoder(MicrowaveActivePacket::encode).decoder(MicrowaveActivePacket::decode).consumerNetworkThread(CONTEXT, MicrowaveActivePacket::handle);
-        PFM_CHANNEL.messageBuilder(ToiletUsePacket.class, ++id).encoder(ToiletUsePacket::encode).decoder(ToiletUsePacket::decode).consumerNetworkThread(CONTEXT, ToiletUsePacket::handle);
-        PFM_CHANNEL.messageBuilder(TrashcanClearPacket.class, ++id).encoder(TrashcanClearPacket::encode).decoder(TrashcanClearPacket::decode).consumerNetworkThread(CONTEXT, TrashcanClearPacket::handle);
-        PFM_CHANNEL.messageBuilder(SyncConfigPacket.class, ++id).encoder(SyncConfigPacket::encode).decoder(SyncConfigPacket::decode).consumerNetworkThread(CONTEXT, SyncConfigPacket::handle);
+        PFM_CHANNEL.messageBuilder(MicrowaveUpdatePacket.class, NetworkDirection.PLAY_TO_CLIENT).encoder(MicrowaveUpdatePacket::encode).decoder(MicrowaveUpdatePacket::decode).consumerNetworkThread(CONTEXT, MicrowaveUpdatePacket::handle).add();
+        PFM_CHANNEL.messageBuilder(MicrowaveActivePacket.class, NetworkDirection.PLAY_TO_SERVER).encoder(MicrowaveActivePacket::encode).decoder(MicrowaveActivePacket::decode).consumerNetworkThread(CONTEXT, MicrowaveActivePacket::handle).add();
+        PFM_CHANNEL.messageBuilder(ToiletUsePacket.class, NetworkDirection.PLAY_TO_SERVER).encoder(ToiletUsePacket::encode).decoder(ToiletUsePacket::decode).consumerNetworkThread(CONTEXT, ToiletUsePacket::handle).add();
+        PFM_CHANNEL.messageBuilder(TrashcanClearPacket.class, NetworkDirection.PLAY_TO_SERVER).encoder(TrashcanClearPacket::encode).decoder(TrashcanClearPacket::decode).consumerNetworkThread(CONTEXT, TrashcanClearPacket::handle).add();
+        PFM_CHANNEL.messageBuilder(SyncConfigPacket.class, NetworkDirection.PLAY_TO_CLIENT).encoder(SyncConfigPacket::encode).decoder(SyncConfigPacket::decode).consumerNetworkThread(CONTEXT, SyncConfigPacket::handle).add();
         //PFM_CHANNEL.registerMessage(++id, ResetConfigPacket.class, ResetConfigPacket::encode, ResetConfigPacket::decode, ResetConfigPacket::handle);
     }
 
@@ -38,10 +40,14 @@ public class NetworkRegistryForge {
             if (PaladinFurnitureMod.getPFMConfig().shouldGiveGuideBook()) {
                 //Give book
                 PFMCriteria.GUIDE_BOOK_CRITERION.trigger((ServerPlayerEntity) event.getEntity());
-
-                //Sync Config
-                NetworkRegistryForge.PFM_CHANNEL.send(new SyncConfigPacket(PaladinFurnitureMod.getPFMConfig().options), PacketDistributor.TRACKING_ENTITY_AND_SELF.with(event.getEntity()));
             }
         }
    }
+
+    @SubscribeEvent
+    public static void onConfigSync(GatherLoginConfigurationTasksEvent event) {
+        event.addTask(new SimpleConfigurationTask(new ServerPlayerConfigurationTask.Key("pfm:sync_config"), (context) -> {
+            NetworkRegistryForge.PFM_CHANNEL.send(new SyncConfigPacket(PaladinFurnitureMod.getPFMConfig().options), context.getConnection());
+        }));
+    }
 }
