a6c48c9c4da7a4c6a05515888ab794ecebc535fc
UnlikePaladin <36827970+UnlikePaladin@users.noreply.github.com>
2023-11-03 14:50:34 -0600

    Rewrote Data Generation to be lazy Allow Immersive Portals mirror spawning and rendering to be disabled Fixed Embur BYG Wood type not being detected

---
+++ common/src/main/java/com/unlikepaladin/pfm/compat/imm_ptl/client/PFMMirrorEntityRenderer.java
@@ -0,0 +1,20 @@
+package com.unlikepaladin.pfm.compat.imm_ptl.client;
+
+import com.unlikepaladin.pfm.PaladinFurnitureMod;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.entity.EntityRendererFactory;
+import net.minecraft.client.util.math.MatrixStack;
+import qouteall.imm_ptl.core.portal.Portal;
+import qouteall.imm_ptl.core.render.PortalEntityRenderer;
+
+public class PFMMirrorEntityRenderer extends PortalEntityRenderer {
+    public PFMMirrorEntityRenderer(EntityRendererFactory.Context context) {
+        super(context);
+    }
+
+    @Override
+    public void render(Portal portal, float yaw, float tickDelta, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int light) {
+        if (PaladinFurnitureMod.getPFMConfig().doImmersivePortalsMirrorsRender())
+            super.render(portal, yaw, tickDelta, matrixStack, vertexConsumerProvider, light);
+    }
+}
---
+++ fabric/src/main/java/com/unlikepaladin/pfm/mixin/fabric/PFMReloadableResourceManagerImplMixin.java
@@ -0,0 +1,32 @@
+package com.unlikepaladin.pfm.mixin.fabric;
+
+import com.google.common.base.Suppliers;
+import com.mojang.bridge.game.PackType;
+import com.unlikepaladin.pfm.client.PathPackRPWrapper;
+import com.unlikepaladin.pfm.runtime.PFMRuntimeResources;
+import net.minecraft.SharedConstants;
+import net.minecraft.resource.ReloadableResourceManagerImpl;
+import net.minecraft.resource.ResourcePack;
+import net.minecraft.resource.metadata.PackResourceMetadata;
+import net.minecraft.text.LiteralText;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Mixin(value = ReloadableResourceManagerImpl.class)
+public class PFMReloadableResourceManagerImplMixin {
+
+    @ModifyVariable(at = @At(value = "HEAD"), method = "reload", argsOnly = true)
+    private List<ResourcePack> createReload(List<ResourcePack> packs) {
+        List<ResourcePack> resourcePacks = new ArrayList<>(packs);
+        resourcePacks.removeIf(pack -> pack instanceof PathPackRPWrapper);
+        PFMRuntimeResources.RESOURCE_PACK_LIST = resourcePacks;
+        PackResourceMetadata packResourceMetadata = new PackResourceMetadata(new LiteralText("pfm-runtime-resources"), SharedConstants.getGameVersion().getPackVersion(PackType.RESOURCE));
+        resourcePacks.add(new PathPackRPWrapper(Suppliers.memoize(() -> {
+            PFMRuntimeResources.prepareAndRunResourceGen(false); return PFMRuntimeResources.ASSETS_PACK;}), packResourceMetadata));
+        return packs;
+    }
+}
