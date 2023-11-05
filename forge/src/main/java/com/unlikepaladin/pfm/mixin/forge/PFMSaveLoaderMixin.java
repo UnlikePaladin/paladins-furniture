8fb3fe5046e688581cc5058674fc3a25bd95400e
UnlikePaladin <36827970+UnlikePaladin@users.noreply.github.com>
2023-11-04 20:00:39 -0600

    Fixed another possible issue with reloading

--- forge/src/main/java/com/unlikepaladin/pfm/mixin/forge/PFMSaveLoaderMixin.java
+++ forge/src/main/java/com/unlikepaladin/pfm/mixin/forge/PFMSaveLoaderMixin.java
@@ -1,14 +1,9 @@
-package com.unlikepaladin.pfm.mixin;
+package com.unlikepaladin.pfm.mixin.forge;
 
-import com.google.common.base.Suppliers;
-import com.mojang.bridge.game.PackType;
 import com.unlikepaladin.pfm.client.PathPackRPWrapper;
 import com.unlikepaladin.pfm.runtime.PFMRuntimeResources;
-import net.minecraft.SharedConstants;
 import net.minecraft.resource.ResourcePack;
-import net.minecraft.resource.metadata.PackResourceMetadata;
 import net.minecraft.server.SaveLoader;
-import net.minecraft.text.LiteralText;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
@@ -21,11 +16,9 @@ public class PFMSaveLoaderMixin {
 
     @ModifyArg(method = "ofLoaded", at = @At(value = "INVOKE", target = "Lnet/minecraft/resource/LifecycledResourceManagerImpl;<init>(Lnet/minecraft/resource/ResourceType;Ljava/util/List;)V"), index = 1)
     private static List<ResourcePack> createReload(List<ResourcePack> packs) {
-        PFMRuntimeResources.RESOURCE_PACK_LIST = packs;
         List<ResourcePack> resourcePacks = new ArrayList<>(packs);
-        PackResourceMetadata packResourceMetadata = new PackResourceMetadata(new LiteralText("pfm-runtime-resources"), SharedConstants.getGameVersion().getPackVersion(PackType.RESOURCE));
-        resourcePacks.add(new PathPackRPWrapper(Suppliers.memoize(() -> {
-            PFMRuntimeResources.prepareAndRunResourceGen(false); return PFMRuntimeResources.ASSETS_PACK;}), packResourceMetadata));
-        return resourcePacks;
+        resourcePacks.removeIf(pack -> pack instanceof PathPackRPWrapper);
+        PFMRuntimeResources.RESOURCE_PACK_LIST = resourcePacks;
+        return packs;
     }
 }
