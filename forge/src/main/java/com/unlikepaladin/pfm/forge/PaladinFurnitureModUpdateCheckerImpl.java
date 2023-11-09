a2fb735bde75752d80418b8e420db6d32cb9ac41
UnlikePaladin <36827970+UnlikePaladin@users.noreply.github.com>
2023-11-09 16:21:38 -0600

    Move update file to the pfm folder

--- forge/src/main/java/com/unlikepaladin/pfm/forge/PaladinFurnitureModUpdateCheckerImpl.java
+++ forge/src/main/java/com/unlikepaladin/pfm/forge/PaladinFurnitureModUpdateCheckerImpl.java
@@ -9,11 +9,6 @@ import java.net.URL;
 import java.nio.file.Path;
 
 public class PaladinFurnitureModUpdateCheckerImpl {
-    public static File getUpdateFile() {
-        Path path = FMLPaths.GAMEDIR.get().resolve("pfmUpdateInfo.json");
-        return path.toFile();
-    }
-
     public static URL getUpdateURL() throws MalformedURLException{
         return new URL("https://github.com/UnlikePaladin/Paladins-Furniture-Update-Index/releases/latest/download/updateIndexForge.json");
     }
