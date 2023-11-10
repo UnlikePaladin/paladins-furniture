6dbf556722049959dcdd5136f8ba6f849c687c59
UnlikePaladin <36827970+UnlikePaladin@users.noreply.github.com>
2023-11-10 02:15:49 -0600

    Fix Lamp Crash on Forge

--- forge/src/main/java/com/unlikepaladin/pfm/client/forge/PFMItemRenderer.java
+++ forge/src/main/java/com/unlikepaladin/pfm/client/forge/PFMItemRenderer.java
@@ -18,8 +18,10 @@ import net.minecraft.client.render.model.BakedModel;
 import net.minecraft.client.render.model.json.ModelTransformation;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.Arm;
 import net.minecraft.util.Identifier;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.ForgeHooksClient;
 import net.minecraftforge.fml.common.Mod;
 
 import java.util.ArrayList;
@@ -34,6 +36,26 @@ public class PFMItemRenderer extends BuiltinModelItemRenderer {
         super(MinecraftClient.getInstance().getBlockEntityRenderDispatcher(), MinecraftClient.getInstance().getEntityModelLoader());
     }
 
+
+    static Map<WoodVariant, Map<String, BakedModel>> bakedModels = new LinkedHashMap<>();
+    static List<String> modelParts = new ArrayList<>();
+    public BakedModel getLampPartFromVariant(WoodVariant variantBase, int index) {
+        if (!bakedModels.isEmpty() && !modelParts.isEmpty()) {
+            return bakedModels.get(variantBase).get(modelParts.get(index));
+        }
+        modelParts.clear();
+        bakedModels.clear();
+        modelParts.addAll(UnbakedBasicLampModel.MODEL_PARTS_BASE);
+        modelParts.addAll(UnbakedBasicLampModel.STATIC_PARTS);
+        for (WoodVariant woodVariant : WoodVariantRegistry.getVariants()) {
+            bakedModels.put(woodVariant, new LinkedHashMap<>());
+            for (String part : modelParts) {
+                bakedModels.get(woodVariant).put(part, MinecraftClient.getInstance().getBakedModelManager().getModel(new Identifier(PaladinFurnitureMod.MOD_ID, part.replaceAll("template", woodVariant.asString()))));
+            }
+        }
+        return bakedModels.get(variantBase).get(modelParts.get(index));
+    }
+
     @Override
     public void render(ItemStack stack, ModelTransformation.Mode mode, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, int overlay) {
         if (stack.isOf(PaladinFurnitureModBlocksItems.BASIC_LAMP_ITEM)) {
@@ -41,30 +63,28 @@ public class PFMItemRenderer extends BuiltinModelItemRenderer {
             if (stack.hasNbt()) {
                 variant = WoodVariantRegistry.getVariant(Identifier.tryParse(stack.getSubNbt("BlockEntityTag").getString("variant")));
             }
-            Map<WoodVariant, Map<String, BakedModel>> bakedModels = new LinkedHashMap<>();
-            List<String> modelParts = new ArrayList<>(UnbakedBasicLampModel.MODEL_PARTS_BASE);
-            modelParts.addAll(UnbakedBasicLampModel.STATIC_PARTS);
-            for (WoodVariant woodVariant : WoodVariantRegistry.getVariants()) {
-                bakedModels.put(woodVariant, new LinkedHashMap<>());
-                for (String part : modelParts) {
-                    bakedModels.get(woodVariant).put(part, MinecraftClient.getInstance().getBakedModelManager().getModel(new Identifier(PaladinFurnitureMod.MOD_ID, part.replaceAll("template", woodVariant.asString()))));
-                }
-            }
+
             boolean glint = stack.hasGlint();
 
             ItemRenderer renderer = MinecraftClient.getInstance().getItemRenderer();
-            matrices.push();
-            BakedModel lampShadeModel = (bakedModels.get(variant).get(modelParts.get(4))).handlePerspective(mode, matrices);
-            BakedModel poleModel = (bakedModels.get(variant).get(modelParts.get(2)));
-            BakedModel bulbModel = (bakedModels.get(variant).get(modelParts.get(5)));
+            boolean leftHanded = MinecraftClient.getInstance().player != null && MinecraftClient.getInstance().player.getMainArm() == Arm.LEFT && mode.isFirstPerson();
+            VertexConsumer consumer = ItemRenderer.getItemGlintConsumer(vertexConsumers, RenderLayers.getItemLayer(stack, true), true, glint);
+
+            matrices.pop();
+
+
+            BakedModel lampShadeModel = ForgeHooksClient.handleCameraTransforms(matrices, getLampPartFromVariant(variant, 4), mode, leftHanded);
 
             matrices.translate(-.5, -.5, -.5); // Replicate ItemRenderer's translation
 
-            VertexConsumer consumer = ItemRenderer.getItemGlintConsumer(vertexConsumers, RenderLayers.getItemLayer(stack, true), true, glint);
             renderer.renderBakedItemModel(lampShadeModel, stack, light, overlay, matrices, consumer);
+
+            BakedModel poleModel = getLampPartFromVariant(variant, 2);
             renderer.renderBakedItemModel(poleModel, stack, light, overlay, matrices, consumer);
+
+            BakedModel bulbModel = getLampPartFromVariant(variant, 5);
             renderer.renderBakedItemModel(bulbModel, stack, light, overlay, matrices, consumer);
-            matrices.pop();
+            matrices.push();
         }
     }
 }
