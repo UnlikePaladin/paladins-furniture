10a5e9bdd871620cd56dd23ed6af70f4d2be2ed9
UnlikePaladin <36827970+UnlikePaladin@users.noreply.github.com>
2025-03-15 23:13:11 -0600

    forgot to make it mutable

--- common/src/main/java/com/unlikepaladin/pfm/mixin/PFMMinecraftClientAcccessor.java
+++ common/src/main/java/com/unlikepaladin/pfm/mixin/PFMMinecraftClientAcccessor.java
@@ -5,6 +5,7 @@ import net.minecraft.client.option.InactivityFpsLimiter;
 import net.minecraft.client.util.tracy.TracyFrameCapturer;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.gen.Accessor;
 import org.spongepowered.asm.mixin.gen.Invoker;
 
@@ -19,6 +20,7 @@ public interface PFMMinecraftClientAcccessor {
     @Accessor("renderTaskQueue")
     Queue<Runnable> getRenderTasks();
 
+    @Mutable
     @Accessor("inactivityFpsLimiter")
     void setInactivityFpsLimiter(InactivityFpsLimiter inactivityFpsLimiter);
 }
