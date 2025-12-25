4768c7b1b1bd6d44076ea72fffa1bd22a8856391
UnlikePaladin <36827970+UnlikePaladin@users.noreply.github.com>
2025-12-24 20:27:08 -0600

    particle fix

--- forge/src/main/java/com/unlikepaladin/pfm/blocks/models/basicLamp/forge/ForgeBasicLampModel.java
+++ forge/src/main/java/com/unlikepaladin/pfm/blocks/models/basicLamp/forge/ForgeBasicLampModel.java
@@ -45,6 +45,15 @@ public class ForgeBasicLampModel extends PFMForgeBakedModel {
         builder.withProperty(VARIANT);
     }
 
+    @Override
+    public void getPropertiesForItem(ItemStack stack, IModelData data) {
+        super.getPropertiesForItem(stack, data);
+        if (stack.hasNbt() && stack.getNbt().contains("BlockEntityTag") && stack.getSubNbt("BlockEntityTag").contains("variant")) {
+            WoodVariant variant = WoodVariantRegistry.getVariant(Identifier.tryParse(stack.getSubNbt("BlockEntityTag").getString("variant")));
+            data.setData(VARIANT, variant);
+        }
+    }
+
     @NotNull
     @Override
     public IModelData getModelData(@NotNull BlockRenderView world, @NotNull BlockPos pos, @NotNull BlockState state, @NotNull IModelData tileData) {
